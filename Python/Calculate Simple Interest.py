2fc48ca5fd588d7643f5be122d73af9de89ad16e
Shaganashree P L <120777273+Shaganashree97@users.noreply.github.com>
2023-12-03 10:44:08 +0530

    Create Calculate Simple Interest.py

---
+++ Python/Calculate Simple Interest.py
@@ -0,0 +1,14 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+#Your code goes here
+
+PA = int(input( ))
+RI = float(input( ))
+T = int(input( ))
+
+SI = PA*RI*T/100
+
+print(int(SI))
